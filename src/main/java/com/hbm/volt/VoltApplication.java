46c862d274829f088ca6838be235c35c05a9a8a1
bha1 <peeyushbhawan@gmail.com>
2017-10-07 10:22:19 +1100

    init commit

---
+++ src/main/java/com/hbm/volt/VoltApplication.java
@@ -0,0 +1,12 @@
+package com.hbm.volt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoltApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoltApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hbm/volt/VoltApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hbm.volt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VoltApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
